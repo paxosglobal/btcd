3b5c56ee9ef7f522bb46764f7ff8bc04ffcb2a07
connorwstein <connor.stein@mail.mcgill.ca>
2020-02-06 18:04:20 -0500

    disable tls

--- integration/rpctest/node.go
+++ integration/rpctest/node.go
@@ -150,7 +150,7 @@ func (n *nodeConfig) rpcConnConfig() rpc.ConnConfig {
 		Pass:                 n.rpcPass,
 		Certificates:         n.certificates,
 		DisableAutoReconnect: true,
-		HTTPPostMode: true,
+		DisableTLS: true,
 	}
 }
 
