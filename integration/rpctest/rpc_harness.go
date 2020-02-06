8ad9ff1a426722b73bbb425de0024f4713f3c216
connorwstein <connor.stein@mail.mcgill.ca>
2020-02-06 15:36:32 -0500

    a log

--- integration/rpctest/rpc_harness.go
+++ integration/rpctest/rpc_harness.go
@@ -102,6 +102,7 @@ func New(activeNet *chaincfg.Params, handlers *rpcclient.NotificationHandlers,
 
 	harnessStateMtx.Lock()
 	defer harnessStateMtx.Unlock()
+	fmt.Println("new rpc harness")
 
 	// Add a flag for the appropriate network type based on the provided
 	// chain params.
